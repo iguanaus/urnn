0432d663da86f01e3ca1ef3cb8d29d0710e3a2e9
iguanaus <johnpeurifoy@yahoo.com>
2016-11-16 16:27:08 -0500

    newnew

--- mnist.py
+++ mnist.py
@@ -195,19 +195,8 @@ def main(argv):
     if not os.path.exists('exp'):
         os.makedirs('exp')
 
-    total_parameters = 0
-    for variable in model.trainable_variables():
-        # shape is an array of tf.Dimension
-        shape = variable.get_shape()
-        print(shape)
-        print(len(shape))
-        variable_parametes = 1
-        for dim in shape:
-            print(dim)
-            variable_parametes *= dim.value
-        print(variable_parametes)
-        total_parameters += variable_parametes
-    print(total_parameters)
+    print (model.summary())
+
 
 
 

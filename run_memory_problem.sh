c1f2486303b22a4a691c04c305271259066f6ee3
Ubuntu <ubuntu@ip-172-31-2-9.us-west-1.compute.internal>
2016-11-28 19:39:48 +0000

    Their paper results

--- run_memory_problem.sh
+++ run_memory_problem.sh
@@ -2,7 +2,7 @@
 niter=3000
 batch_size=20
 learning_rate=0.001
-
+n_cats=8
 mkdir -p exp
 
 for t in 1000 2000; do
@@ -10,9 +10,9 @@ for t in 1000 2000; do
         SECONDS=0
         w_impl=$3
         echo "Running memory_problem experiment for N=$2 $1 with time_steps=$t"
-        cmd="THEANO_FLAGS='device=gpu0' python2.7 -u memory_problem.py $niter ${batch_size} $2 $t 0.001 exp/memory_problem_$1_$3_$1_nhidden$2_t$t $1 categorical True CE $w_impl"
+        cmd="THEANO_FLAGS='device=gpu0' python2.7 -u memory_problem.py $niter ${batch_size} $2 $t 0.001 exp/memory_problem_$1_$3_$1_nhidden$2_t$t $1 categorical True CE $w_impl $n_cats"
         echo $cmd
-        #eval $cmd
+        eval $cmd
         echo "Experiment took $SECONDS seconds."
     done
 done

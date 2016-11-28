7ef8e2871cf79654b59ec948b6ffc8dc92e35a8d
iguanaus <johnpeurifoy@yahoo.com>
2016-11-28 01:30:59 -0500

    BengioandLSTM1000 ready

--- run_memory_bengio_1000.sh
+++ run_memory_bengio_1000.sh
@@ -3,12 +3,12 @@ for tval in 1000
 do
 niter=10000
 batch_size=128 
-n_hidden=40
+n_hidden=128
 time_steps=$tval
 learning_rate=0.001
 num_cats=5
-savefile='exp/memory_problem_adhoc_lstms_learning_0.001_ncats_'$num_cats'_nhidden40_t'$time_steps
-model='LSTM'
+savefile='exp/memory_problem_adhoc_complex_RNN_learning_0.001_ncats_'$num_cats'_nhidden40_t'$time_steps
+model='complex_RNN'
 input_type='categorical'
 out_every_t='True'
 loss_function='CE'

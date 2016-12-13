d4acff78a779f9cb47655bafa7ce83743c5c3a7c
iguanaus <johnpeurifoy@yahoo.com>
2016-12-13 15:39:46 -0500

    mycalrunready fixed args 2

--- run_urnn_full_lstm_t_20.sh
+++ run_urnn_full_lstm_t_20.sh
@@ -12,7 +12,7 @@ for t in 20; do
         SECONDS=0
         w_impl=$3
         echo "Running memory_problem experiment for N=$2 $1 with time_steps=$t"
-        cmd="THEANO_FLAGS='device=gpu0' python2.7 -u memory_problem.py --n_iter $niter --n_batch ${batch_size} --n_hidden $2 --time_steps $t --learning_rate 0.001 --savefile exp/memory_problem_$1_$3_$1_nhidden$2_t$t --model $1 --input_type categorical --out_every_t True --loss_function CE --n_reflections $w_impl --num_cats $n_cats"
+        cmd="THEANO_FLAGS='device=gpu0' python2.7 -u memory_problem.py --n_iter $niter --n_batch ${batch_size} --n_hidden $2 --time_steps $t --learning_rate 0.001 --savefile exp/memory_problem_$1_$3_$1_nhidden$2_t$t --model $1 --input_type categorical --out_every_t True --loss_function CE --w_impl $w_impl --num_cats $n_cats"
         echo $cmd
         eval $cmd
         echo "Experiment took $SECONDS seconds."
